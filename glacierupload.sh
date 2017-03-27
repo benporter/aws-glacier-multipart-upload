848a22f36c80aac2ef8bc0cf657714c664e43263
gardnert <gardnert@vt.edu>
2017-03-27 16:17:27 -0400

    Update glacierupload.sh
    The script now requires you to pass the name of the whole archive in as an argument
    Fixed bytesize being wrong if it was bigger than the final part of the upload
    changed abort-multipart-upload to complete-multipart-upload
    added treehash computation and inclusion in complete-multipart-upload
    store the json response from amazon in result.json

--- glacierupload.sh
+++ glacierupload.sh
@@ -5,7 +5,8 @@
 # sudo dnf install parallel
 # sudo pip install awscli
 
-byteSize=4194304
+byteSize=2147483648
+vaultName='backups'
 
 # count the number of files that begin with "part"
 fileCount=$(ls -1 | grep "^part" | wc -l)
@@ -15,7 +16,7 @@ echo "Total parts to upload: " $fileCount
 files=$(ls | grep "^part")
 
 # initiate multipart upload connection to glacier
-init=$(aws glacier initiate-multipart-upload --account-id - --part-size $byteSize --vault-name media1 --archive-description "November 2015 Pictures and Videos")
+init=$(aws glacier initiate-multipart-upload --account-id - --part-size $byteSize --vault-name $vaultName --archive-description 'Upload Description')
 
 echo "---------------------------------------"
 # xargs trims off the quotes
@@ -25,13 +26,22 @@ uploadId=$(echo $init | jq '.uploadId' | xargs)
 # create temp file to store commands
 touch commands.txt
 
+#get total size in bytes of the archive
+archivesize=`ls -l $1 | cut -d ' ' -f 8`
+
 # create upload commands to be run in parallel and store in commands.txt
 i=0
 for f in $files 
   do
+     filesize=`ls -l $f | cut -d ' ' -f 8`
+     echo 'filesize '$filesize
      byteStart=$((i*byteSize))
      byteEnd=$((i*byteSize+byteSize-1))
-     echo aws glacier upload-multipart-part --body $f --range "'"'bytes '"$byteStart"'-'"$byteEnd"'/*'"'" --account-id - --vault-name media1 --upload-id $uploadId >> commands.txt
+     #if the filesize is less than the bytesize, set the bytesize to be the filesize
+     if [ $byteEnd -gt $filesize ]; then
+        byteEnd=$((filesize-1))
+     fi
+     echo aws glacier upload-multipart-part --body $f --range "'"'bytes '"$byteStart"'-'"$byteEnd"'/*'"'" --account-id - --vault-name $vaultName --upload-id $uploadId >> commands.txt
      i=$(($i+1))
      
   done
@@ -45,16 +55,23 @@ parallel --load 100% -a commands.txt --no-notice --bar
 
 echo "List Active Multipart Uploads:"
 echo "Verify that a connection is open:"
-aws glacier list-multipart-uploads --account-id - --vault-name media1
+aws glacier list-multipart-uploads --account-id - --vault-name $vaultName
+
+#compute the tree hash
+checksum=`java TreeHashExample $1 | cut -d ' ' -f 5`
 
 # end the multipart upload
-aws glacier abort-multipart-upload --account-id - --vault-name media1 --upload-id $uploadId
+result=`aws glacier complete-multipart-upload --account-id - --vault-name $vaultName --upload-id $uploadId --archive-size $archivesize --checksum $checksum`
+
+#store the json response from amazon for record keeping
+touch result.json
+echo $result >> result.json
 
 # list open multipart connections
 echo "------------------------------"
 echo "List Active Multipart Uploads:"
 echo "Verify that the connection is closed:"
-aws glacier list-multipart-uploads --account-id - --vault-name media1
+aws glacier list-multipart-uploads --account-id - --vault-name $vaultName
 
 #echo "-------------"
 #echo "Contents of commands.txt"
@@ -62,5 +79,4 @@ aws glacier list-multipart-uploads --account-id - --vault-name media1
 echo "--------------"
 echo "Deleting temporary commands.txt file"
 rm commands.txt
-
 
